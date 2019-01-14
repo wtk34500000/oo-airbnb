2664011ad543d23a14727df55f0a4035d35d501d
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-14 16:18:12 -0500

    add some inputs for testing

--- tools/console.rb
+++ tools/console.rb
@@ -5,7 +5,23 @@ def reload
 end
 
 
+la=Listing.new("LA")
+ca=Listing.new("CA")
+pa=Listing.new("PA")
+tx=Listing.new("TX")
 
+john=Guest.new("John")
+lisa=Guest.new("Lisa")
+ela=Guest.new("Ela")
+alex=Guest.new("Alex")
+
+trip1=Trip.new(john, la, 20)
+trip2=Trip.new(lisa, la, 30)
+trip3=Trip.new(ela, tx, 50)
+trip4=Trip.new(alex, pa, 10)
+trip5=Trip.new(john, ca, 20)
+
+Listing.most_popular
 
 
 # Put your variables here~!
